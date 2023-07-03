6b053035bd5f6ec90ba98e50f6e49c0d3ac68acc
Parth Gupta <guptaparth716@gmail.com>
2023-07-03 20:16:07 +0530

    Updated settings.py

--- Travelista/settings.py
+++ Travelista/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'travelista-data',
         'USER': 'postgres',
-        'PASSWORD': 'Praveen1@',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
