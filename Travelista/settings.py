b485636805fbbb3420bbde8718830bd8a452f53c
Parth Gupta <guptaparth716@gmail.com>
2023-07-21 14:28:48 +0530

    updated allowed hosts

--- Travelista/settings.py
+++ Travelista/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '#xhvp)cbm$4!y-s6ry4jmdmdt!@vn_&(x97n%4=tm(qicyjq2x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
